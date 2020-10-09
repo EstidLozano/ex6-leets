c1927bf8a8e63f7499c63f3cdb24046cc6ba4253
Estid Felipe Lozano Reyes <36607963+EstidLozano@users.noreply.github.com>
2020-10-09 13:45:53 -0500

    1er commit

---
+++ src/utils/Constantes.java
@@ -0,0 +1,30 @@
+package utils;
+
+public class Constantes {
+	
+	//Spaces
+	
+	/**
+	 * Token spaces
+	 */
+	public static String SPACES_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkX3NpZyI6IlBoakhkd0RpZEJJaDA5QjljaWhMbXFiaWlncmFISV9IUUZEV1lmeHo3cXMiLCJwcm9kdWN0X3R5cGUiOiJhY2NvdW50cyIsImxhc3R1cGRhdGV0aW1lIjoiMjAyMC0xMC0wM1QxNjowNzoyOS40NzYiLCJpc3MiOiJhdmF5YWNsb3VkLmNvbSIsInB1YmxpY2tleWlkIjoiYWd4emZtOXVaWE51WVRJd01UUnlHZ3NTRFVkS2QzUlFkV0pzYVdOTFpYa1lnSUN3N1BpY2lna00iLCJleHAiOjE2MDQ4NTYyNjQsInVzZXJfaWQiOiJhZ3h6Zm05dVpYTnVZVEl3TVRSeUVRc1NCRlZ6WlhJWWdJQ3duS3kxdEFrTSIsInZlciI6IjIuMCJ9.KCRKXyY-OvmLs7980S9kVqGLbUrIjB44DVwu4XqiHs7o56q1j93P8lodBAoxM5eUjh58RVa0Ah8N_XChe3XJ3ZTgN1P1u-UyxddTg0Sahi6eDCelboTWpJccWG1avmyn9jD-r1j6siAmJkt2wng0kZ9N1bwolQesI-99FxmPljjoYpfpFG6ieBghW3G1vBR4288b0FpIMU6toHz3oAw0pLOZdLtfrA06my7DWBvajFNBXH_wL9ugeL1snfP3m9RqhNRWcvIlfKeOZHRBLCykp6Pc8sN557vfubwnH-tX-IGpqi2xC9ODV7GTg77j1h3tKQvspbZIv37wFTHR9QiYCA";
+	
+	/**
+	 * Id del spacio donde se publicaran los chat/tasks
+	 */
+	public static String SPACE_ID = "5f725fac3c208b159773bc8d";
+	
+	
+	//Dialogflow
+	
+	/**
+	 * Project id del agente de Dialogflow, debe coincidir con el projectId que se encuentra en el json.key de resources
+	 */
+	public static String PROJETC_ID = "hack2020-2-leets";
+	
+	/**
+	 * Bot name
+	 * */
+	public static String BOT = "@GRUPO2";
+
+}
